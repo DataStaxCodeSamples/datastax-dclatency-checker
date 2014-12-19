bb834558625d3b68bf1019b1fa87abfa609fda61
Patrick Callaghan <patcho@gmail.com>
2014-12-19 00:01:34 +0000

    updated for testing

---
+++ README.md
@@ -0,0 +1,4 @@
+To run the test, run the following
+```
+mvn clean compile exec:java -Dexec.mainClass="com.datastax.test.Main"
+```
--- src/main/java/com/datastax/test/Main.java
+++ src/main/java/com/datastax/test/Main.java
@@ -20,21 +20,20 @@ public class Main {
 		Session session = cluster.connect();
 		
 		//Create keyspace and table 
-		String createKeyspace = "create KEYSPACE if not exists datastax-testing WITH replication = {'class': 'NetworkTopologyStrategy' "
+		String createKeyspace = "create KEYSPACE if not exists datastax_testing WITH replication = {'class': 'NetworkTopologyStrategy', "
 				+ "'"+ localDC + "': 1, '" + remoteDC + "': 1};";
-		String createTable = "create table if not exists datastax-testing.latency (id text PRIMARY KEY)";
+		String createTable = "create table if not exists datastax_testing.latency (id text PRIMARY KEY)";
 
-		String INSERT = "insert into datastax-testing.latency (id) values (?)";   
-		
-		PreparedStatement stmtLocal = session.prepare(INSERT);
-		PreparedStatement stmtRemote = session.prepare(INSERT);
-		
-		stmtLocal.setConsistencyLevel(ConsistencyLevel.ONE);
-		stmtRemote.setConsistencyLevel(ConsistencyLevel.QUORUM);
-				
+		String INSERT = "insert into datastax_testing.latency (id) values (?)";   
+						
 		session.execute(createKeyspace);
 		session.execute(createTable);
 		
+		PreparedStatement stmtLocal = session.prepare(INSERT);
+		PreparedStatement stmtRemote = session.prepare(INSERT);
+		stmtLocal.setConsistencyLevel(ConsistencyLevel.ONE);
+		stmtRemote.setConsistencyLevel(ConsistencyLevel.QUORUM);
+
 		int counter = 0;
 		
 		while (true){
