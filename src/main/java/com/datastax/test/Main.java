c4b5b795d9270dc2ab27434d47898a1161a41b07
Patrick Callaghan <patcho@gmail.com>
2014-12-19 09:22:58 +0000

    update with exception handling

--- src/main/java/com/datastax/test/Main.java
+++ src/main/java/com/datastax/test/Main.java
@@ -6,74 +6,80 @@ import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ConsistencyLevel;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.exceptions.WriteTimeoutException;
 import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
 
 /**
  */
 public class Main {
 
-	public Main(){
+	public Main() {
 		String contactPointsStr = PropertyHelper.getProperty("contactPoints", "localhost");
 		String localDC = PropertyHelper.getProperty("localdc", "Cassandra");
 		String remoteDC = PropertyHelper.getProperty("remotedc", "Analytics");
-		
+
 		Cluster cluster = Cluster.builder().addContactPoints(contactPointsStr.split(","))
-				.withLoadBalancingPolicy(new DCAwareRoundRobinPolicy(localDC))
-				.build();
-		
+				.withLoadBalancingPolicy(new DCAwareRoundRobinPolicy(localDC)).build();
+
 		Session session = cluster.connect();
-		
-		//Create keyspace and table 
+
+		// Create keyspace and table
 		String createKeyspaceLocal = "create KEYSPACE if not exists datastax_testing_local WITH replication = {'class': 'NetworkTopologyStrategy', "
-				+ "'"+ localDC + "': 1 };";
+				+ "'" + localDC + "': 1 };";
 		String createKeyspaceRemote = "create KEYSPACE if not exists datastax_testing_remote WITH replication = {'class': 'NetworkTopologyStrategy', "
-				+ "'"+ remoteDC + "': 1 };";
+				+ "'" + remoteDC + "': 1 };";
 		String createTableLocal = "create table if not exists datastax_testing_local.latency (id text PRIMARY KEY)";
 		String createTableRemote = "create table if not exists datastax_testing_remote.latency (id text PRIMARY KEY)";
 
 		String INSERT_LOCAL = "insert into datastax_testing_local.latency (id) values (?)";
 		String INSERT_REMOTE = "insert into datastax_testing_remote.latency (id) values (?)";
-						
+
 		session.execute(createKeyspaceLocal);
 		session.execute(createTableLocal);
 		session.execute(createKeyspaceRemote);
 		session.execute(createTableRemote);
-		
+
 		PreparedStatement stmtLocal = session.prepare(INSERT_LOCAL);
 		PreparedStatement stmtRemote = session.prepare(INSERT_REMOTE);
-		
+
 		stmtLocal.setConsistencyLevel(ConsistencyLevel.ONE);
 		stmtRemote.setConsistencyLevel(ConsistencyLevel.ONE);
 
 		int counter = 0;
 		long localTotal = 0;
 		long remoteTotal = 0;
-		
+
 		MovingAverage localMA = new MovingAverage(20);
 		MovingAverage remoteMA = new MovingAverage(20);
-		
-		while (true){
-			
+
+		while (true) {
 			Timer local = new Timer();
-			local.start();
-			session.execute(stmtLocal.bind("" + System.currentTimeMillis()));
-			local.end();
-			long localWrite = local.getTimeTakenMillis();
-			
 			Timer remote = new Timer();
-			remote.start();
-			session.execute(stmtRemote.bind("" + System.currentTimeMillis()));
-			remote.end();
-			long remoteWrite = remote.getTimeTakenMillis();
-			
+
+			try {
+				local.start();
+				session.execute(stmtLocal.bind("" + System.currentTimeMillis()));
+				local.end();
+
+				remote.start();
+				session.execute(stmtRemote.bind("" + System.currentTimeMillis()));
+				remote.end();
+				
+			} catch (WriteTimeoutException e) {
+				System.out.println("Caught exception - " + e.getMessage());
+				continue;
+			}
+
 			counter++;
-			
+			long localWrite = local.getTimeTakenMillis();
+			long remoteWrite = remote.getTimeTakenMillis();
+
 			localMA.newNum(localWrite);
 			remoteMA.newNum(remoteWrite);
-			
-			System.out.println("Local :" + localWrite + "ms Remote :" + remoteWrite + "ms Average local " + localMA.getAvg() 
-					+ " Average Remote " + remoteMA.getAvg());
-	
+
+			System.out.println("Local :" + localWrite + "ms Remote :" + remoteWrite + "ms Average local "
+					+ localMA.getAvg() + " Average Remote " + remoteMA.getAvg());
+
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
@@ -81,7 +87,7 @@ public class Main {
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		new Main();
 	}
